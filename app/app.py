ca2c453daf4022f1481aaeb144324df72e496150
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-07 20:21:10 +0100

    Landing page

---
+++ app/app.py
@@ -0,0 +1,10 @@
+"""Entry point of the application"""
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
