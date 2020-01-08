f6a119e70065f7d4681d0a3138f9d76dcb227359
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-08 23:58:16 +0100

    Register form

--- app/account_routes.py
+++ app/account_routes.py
@@ -1,10 +1,21 @@
 """Routes connected to users accounts"""
 
-from flask import Blueprint
+from flask import Blueprint, render_template, flash, redirect, url_for, request, session
+from forms import RegisterForm
 
 users = Blueprint('account', __name__, template_folder='templates')
 
 
-@users.route('/register')
+@users.route('/register', methods=['GET', 'POST'])
 def register():
-    return 'Register here'
+    form = RegisterForm(meta={'csrf_context': session})
+    if form.validate_on_submit():
+        flash('Konto zostało utworzone', 'alert-success')
+        # TODO: Create user account
+        redirect(url_for('index'))
+
+    print('Form invalid')
+    print(form.login.errors)
+    print(form)
+    print(form.login.errors)
+    return render_template('register.html', form=form)
--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,40 @@
-from wtforms import StringField, PasswordField, ValidationError, FileField, Form
-from wtforms.validators import DataRequired, Email
+from flask_wtf.form import FlaskForm
+from wtforms import StringField, PasswordField, ValidationError, FileField, Form, IntegerField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 from wtforms.csrf.session import SessionCSRF
 from datetime import timedelta
-from flask import current_app
+from config import Config
 
 
-class BaseForm(Form):
+class BaseForm(FlaskForm):
     class Meta:
         csrf = True
         csrf_class = SessionCSRF
-        csrf_secret = current_app.config.SECRET_KEY.encode()
+        csrf_secret = Config.SECRET_KEY.encode()
         csrf_time_limit = timedelta(minutes=20)
 
 
 class RegisterForm(BaseForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    login = StringField('login', validators=[
+        DataRequired('Brak loginu'),
+        # TODO: Check if login is free
+        Length(min=4, message='Login musi mieć co najmniej 4 znaki')
+    ])
+
+    password = PasswordField('password', validators=[
+        DataRequired('Brak hasła'),
+        Length(min=6, message='Hasło musi mieć co najmniej 6 znaków')
+    ])
+    password2 = PasswordField('Password', validators=[
+        # TODO: Check if mail is free
+        EqualTo('password', 'Hasła się różnią')
+    ])
+
+    email = StringField('Mail', validators=[
+        DataRequired('Brak maila'),
+        Email('Nieprawidłowy mail')
+    ])
+
+    lucky_number = IntegerField('Lucky number', validators=[
+        DataRequired('Brak szczęśliwej liczby :(')
+    ])
