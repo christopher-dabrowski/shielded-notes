f4e1e4be9a9100aa0a99f540b8e88acfa2abcbbc
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-12 18:02:22 +0100

    Add spinner for wating for mail send

---
+++ app/static/js/recover_password.js
@@ -0,0 +1,20 @@
+const addSpinner = () => {
+    if (document.getElementById('loading-spinner'))
+        return;
+
+    const targetElement = document.getElementById('submit-button');
+    const spinner = document.createElement('i');
+    spinner.classList.add('fas');
+    spinner.classList.add('fa-spinner');
+    spinner.classList.add('ml-1')
+    spinner.id = 'loading-spinner';
+
+    targetElement.appendChild(spinner);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
+    form.addEventListener("submit", addSpinner);
+});
+
+'<i class="fas fa-spinner"></i>'
